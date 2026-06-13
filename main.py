bbd8943eb4844c8467710f0c8d1d39ae45d79e68
dakshthakkar20-lang <dakshthakkar20@gmail.com>
2026-06-13 14:19:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,5 +89,5 @@ def index():
 if __name__ == "__main__":
     t = threading.Thread(target=run_scanner, daemon=True)
     t.start()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
